eef4b589a101e03d3452be7cae2bd03ff9e32026
Daniel Perez <daniel@perez.sh>
2025-12-08 15:17:14 +0000

    Add day 7

--- day7.py
+++ day7.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from utils import open_input
 
 
@@ -6,19 +7,21 @@ with open_input(7) as f:
     splitters = {
         y: {x for x, c in enumerate(line) if c == "^"} for y, line in enumerate(lines)
     }
-    initial_beams = {lines[0].index("S")}
+    initial_beam = lines[0].index("S")
 
-beams = initial_beams
+beams_count = defaultdict(int, {initial_beam: 1})
 splits = 0
+possibilities = 1
 for y in range(1, max(splitters) + 1):
-    new_beams = set()
-    for beam in beams:
-        if beam in splitters[y]:
+    new_beams = beams_count.copy()
+    for beam, count in beams_count.items():
+        if beam in splitters[y] and count > 0:
             splits += 1
-            new_beams.add(beam - 1)
-            new_beams.add(beam + 1)
-        else:
-            new_beams.add(beam)
-    beams = new_beams
+            possibilities += count
+            new_beams[beam - 1] += count
+            new_beams[beam + 1] += count
+            new_beams[beam] -= count
+    beams_count = new_beams
 
 print(f"Part 1: {splits}")
+print(f"Part 2: {possibilities}")
